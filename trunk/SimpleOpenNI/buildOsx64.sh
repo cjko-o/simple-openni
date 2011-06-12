cc59c062fcb7fc00338ebb5ebc73c7f7c5eadfde
mrn@paus.ch <mrn@paus.ch@81cd9421-9946-bb5f-ad71-2db49e6c0e5f>
2011-06-12 17:21:42 +0000

    - added osx build script

--- trunk/SimpleOpenNI/buildOsx64.sh
+++ trunk/SimpleOpenNI/buildOsx64.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # --------------------------------------------------------------------------
-# buildscript for linux 64bit
+# buildscript for osx 32bit/64bit
 # --------------------------------------------------------------------------
 # Processing Wrapper for the OpenNI/Kinect library
 # http://code.google.com/p/simple-openni
@@ -9,32 +9,34 @@
 # date:  06/11/2011 (m/d/y)
 # ----------------------------------------------------------------------------
 
-# optional, but gives a clean buil
-rm -r build64
+# optional, but gives a clean build
+rm -r build
 
 # check if build folder exists
-if [ ! -d "build64" ]; then
-    mkdir build64
+if [ ! -d "build" ]; then
+    mkdir build
 fi
 
-cd ./build64
+cd ./build
 
 echo "--- generate cmake ---"
-cmake -DOPEN_NI_INCLUDE=/usr/include/openni/ \
+cmake -DOPEN_NI_INCLUDE=/usr/include/ni/ \
+	  -DOPEN_NI_LIB=/usr/lib/ \
 	  -DXN_NITE_INCLUDE=/usr/include/nite/ \
 	  -DXN_NITE_LIB=/usr/lib/ \
-	  -DEIGEN3D_INCLUDE=/usr/include/eigen3/ \
-	  -DBOOST_ROOT=~/Documents/development/libs/boost/boost_1_46_1/ \
-	  -DBoost_LIBRARYDIR=/usr/local/lib/ \
-	  -DP5_JAR=~/Documents/localApps/processing-1.5.1/lib/core.jar \
+	  -DEIGEN3D_INCLUDE=/usr/local/include/eigen3/ \
+	  -DP5_JAR=/Applications/Processing.app/Contents/Resources/Java/core.jar \
+	  -DCMAKE_OSX_ARCHITECTURES="i386;x86_64" \
 	  ..
 
+#	  -DBOOST_ROOT=~/Documents/development/libs/boost/boost_1_46_1/ \
+#	  -DBOOST_LIBRARYDIR=/usr/local/lib/ \
 
 echo "--- build ---"
 # build with 6 threads, verbose is optional, but otherwise you can't see the compiler directives
-# make -j 6 VERBOSE=1
-make -j 6
+make -j 6 VERBOSE=1
+
 
 echo "--- copy ---"
-cp SimpleOpenNI.jar ~/sketchbook/libraries/SimpleOpenNI/library/
-cp libSimpleOpenNI.so ~/sketchbook/libraries/SimpleOpenNI/library/
+cp SimpleOpenNI.jar ~/Documents/Processing/libraries/SimpleOpenNI/library
+cp libSimpleOpenNI.jnilib ~/Documents/Processing/libraries/SimpleOpenNI/library
